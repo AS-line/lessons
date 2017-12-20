e0d7ca30c0ef8e428ef2fefce962344fb6aa497b
As-line <32175896+AS-line@users.noreply.github.com>
2017-12-20 18:04:31 +0300

    Add files via upload

--- sql_project/sql_project/program.py
+++ sql_project/sql_project/program.py
@@ -1,9 +1,9 @@
 import tkinter
 import sqlite3
 
-window = tkinter.Tk()
-window.title('Авторизация')
-window.geometry('800x420')
+root = tkinter.Tk()
+root.title('Авторизация')
+root.geometry('800x420')
 
 
 def log_in(user_login, user_password):
@@ -16,36 +16,28 @@ def log_in(user_login, user_password):
 	check_profile = (user_login, user_password)
 	if check_profile in profiles:
 		txt_label = tkinter.Label(text='YOU HAVE SUCCESSFULLY LOGGED', width=34)
-		txt_label.place(x=20, y=70)
+		txt_label.place(x=20, y=115)
 	else:
 		txt_label = tkinter.Label(text='INCORRECT USERNAME OR PASSWORD')
-		txt_label.place(x=20, y=70)
+		txt_label.place(x=20, y=115)
 
 	descriptor.commit()
 	runner.close()
 	descriptor.close()
 
+def root_deleted():
+    root.quit()
+
+def stwich_root():
+
+	root = tkinter.Tk()
+	root.title('Регистрация')
+	root.geometry('800x420')
+
+	root.mainloop()
 
 
 def registration():
-	window.quit()
-
-
-	window = tkinter.Tk()
-	window.title('Авторизация')
-	window.geometry('800x420')
-
-
-	descriptor = sqlite3.connect('DATA.db')
-	runner = descriptor.cursor()
-
-	idmeter = open("fileid", "r")
-	usid = int(idmeter.read())
-	usid += 1
-	idmeter.close()
-	idmeter = open('fileid', 'w')
-	idmeter.write(str(usid))
-	idmeter.close()
 
 	query = "INSERT INTO profiles_data VALUES (?,?,?,?,?,?,?,?,?)"
 	date = usid,input('Имя:'),input('Фамилия: '), input("Логин: "), input("Пароль: "),None,None,None,None
@@ -71,6 +63,7 @@ def show_profile(true_login):
 	descriptor.close()
 
 
+
 def enter_data(true_login):
 	descriptor = sqlite3.connect('DATA.db')
 	runner = descriptor.cursor()
@@ -84,25 +77,29 @@ def enter_data(true_login):
 	descriptor.close()
 
 
-label_login = tkinter.Label(window, text='Login:')
+label_login = tkinter.Label(root, text='Login:')
 label_login.place(x=0, y=0)
 
-entry_login = tkinter.Entry(window)
+entry_login = tkinter.Entry(root)
 entry_login.place(x=0, y=20)
 
-label_password = tkinter.Label(window, text='Password:')
+label_password = tkinter.Label(root, text='Password:')
 label_password.place(x=140, y=0)
 
-entry_password = tkinter.Entry(window)
+entry_password = tkinter.Entry(root)
 entry_password.place(x=140, y=20)
 
-btn_log_in = tkinter.Button(window, text='Log in', width=36, bg='red')
+btn_log_in = tkinter.Button(root, text='Log in', width=36, bg='red')
 btn_log_in.bind('<Button-1>', lambda event: log_in(str(entry_login.get()), str(entry_password.get())))
-btn_log_in.place(x=2, y=40)
+btn_log_in.place(x=2, y=45)
 
-btn_reg_in = tkinter.Button(window, text='Or registration', width=36, bg='red')
-btn_reg_in.bind('<Button-1>', lambda evnt: registration())
-btn_reg_in.place(x=2, y=65)
+btn_reg_in = tkinter.Button(root, text='Or registration', width=36, bg='red')
+
+btn_reg_in.bind('<Button-1>', lambda evnt: root_deleted())
+btn_reg_in.bind('<Button-1>', lambda evnt: stwich_root())
+# btn_reg_in.bind('<Button-1>', lambda evnt:)
+btn_reg_in.place(x=2, y=75)
 
 
-window.mainloop()
+	
+root.mainloop()
